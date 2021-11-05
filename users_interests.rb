561e52fa6afc55a62159cfd73246d29285081daf
Joe Webster <joseph.scott.webster@gmail.com>
2021-11-05 08:38:13 -0400

    Hom page redirects to a page that lists users

--- users_interests.rb
+++ users_interests.rb
@@ -6,5 +6,15 @@ require 'sinatra/reloader'
 require 'yaml'
 
 get '/' do
-  'HELLO WORLD'
+  redirect '/users'
+end
+
+get '/users' do
+  @users = YAML.load_file('data/users.yaml')
+  @user_names = @users.keys
+
+  erb :users
+end
+
+get '/users/:name' do
 end
