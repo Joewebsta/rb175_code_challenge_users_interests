57ec9415d984029692a504da658e5c832d37990a
Joe Webster <joseph.scott.webster@gmail.com>
2021-11-05 10:20:38 -0400

    Create layout and add user/interest count to botom of page

--- users_interests.rb
+++ users_interests.rb
@@ -7,6 +7,8 @@ require 'yaml'
 
 before do
   @users = YAML.load_file('data/users.yaml')
+  @user_count = @users.keys.size
+  @interest_count = count_interests
 end
 
 get '/' do
@@ -15,6 +17,7 @@ end
 
 get '/users' do
   @user_names = @users.keys
+  @title = 'Users'
   erb :users
 end
 
@@ -24,11 +27,16 @@ get '/users/:name' do
   @email = user_info[:email]
   @interests = user_info[:interests].join(', ')
   @other_users = remove_current_user
+  @title = @name.capitalize
   erb :user
 end
 
 helpers do
   def remove_current_user
     @users.keys.map(&:to_s).reject { |other_name| other_name == @name }
+  end
+
+  def count_interests
+    @users.values.map { |user_info| user_info[:interests] }.flatten.count
   end
 end
