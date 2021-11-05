9fb1990a9b8e1356842360c894d9419422188648
Joe Webster <joseph.scott.webster@gmail.com>
2021-11-05 09:12:37 -0400

    Display user info on user page

--- users_interests.rb
+++ users_interests.rb
@@ -5,18 +5,23 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'yaml'
 
+before do
+  @users = YAML.load_file('data/users.yaml')
+end
+
 get '/' do
   redirect '/users'
 end
 
 get '/users' do
-  @users = YAML.load_file('data/users.yaml')
-  @user_names = @users.keys.map(&:capitalize)
-
+  @user_names = @users.keys
   erb :users
 end
 
 get '/users/:name' do
-  # params[:name]
+  @name = params[:name]
+  user_info = @users[@name.to_sym]
+  @email = user_info[:email]
+  @interests = user_info[:interests].join(', ')
   erb :user
 end
