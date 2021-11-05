78f56782d695522376819add9e3dbe53b6f37025
Joe Webster <joseph.scott.webster@gmail.com>
2021-11-05 09:28:14 -0400

    Display links to other user pages

--- users_interests.rb
+++ users_interests.rb
@@ -23,5 +23,12 @@ get '/users/:name' do
   user_info = @users[@name.to_sym]
   @email = user_info[:email]
   @interests = user_info[:interests].join(', ')
+  @other_users = remove_current_user
   erb :user
+end
+
+helpers do
+  def remove_current_user
+    @users.keys.map(&:to_s).reject { |other_name| other_name == @name }
+  end
 end
